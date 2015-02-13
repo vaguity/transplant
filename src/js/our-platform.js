bcf19e53bd4644b1cc4538a635d61152802169d3
Sean Connolly <sean@percolate.com>
2015-02-13 16:46:38 -0500

    Switch the our-platform architecture to just be panels

--- src/js/our-platform.js
+++ src/js/our-platform.js
@@ -1,49 +1,10 @@
-var panels = [
-	{
-		id: '#customize',
-		top: null,
-	},
-	{
-		id: '#planning',
-		top: null,
-	},
-	{
-		id: '#briefing',
-		top: null,
-	},
-	{
-		id: '#sourcing',
-		top: null,
-	},
-	{
-		id: '#production',
-		top: null,
-	},
-	{
-		id: '#distribution',
-		top: null,
-	},
-	{
-		id: '#monitoring',
-		top: null,
-	},
-	{
-		id: '#analysis',
-		top: null,
-	},
+var fullFrameSelectors = [
+	'.body-container .image.left',
+	'.content.our-platform',
+	'.content.our-platform > .container',
 ];
 
-var panelsLength = panels.length;
-
-var panelSetParameters = {
-	'container': '.content.our-platform',
-	'selectors': '.hero-container .mask, .hero-container .hero, .body-container .image.left',
-};
-
 var vertCenterSelectors = [
-	'.hero .copy.left',
-	'.hero .copy.right',
-	'.our-platform-sections',
 	'#customize .copy.right',
 	'#planning .copy.right',
 	'#briefing .copy.right',
@@ -52,9 +13,13 @@ var vertCenterSelectors = [
 	'#distribution .copy.right',
 	'#monitoring .copy.right',
 	'#analysis .copy.right',
+	{
+		'selector': '.hero-container .mask',
+		'offset': '.header-container',
+	},
 ];
 
-function verticalCenter(selectors, reset) {
+function setVerticalCenter(selectors, reset) {
 
 	var vertCenterLength = selectors.length;
 	var vertCenterPad = 0;
@@ -69,220 +34,79 @@ function verticalCenter(selectors, reset) {
 	var windowHeight = $(this).height();
 
 	for (var i = 0; i < vertCenterLength; i++) {
+		if (typeof selectors[i] === 'object') {
+			vertCenterPad = (windowHeight - $(selectors[i]['selector']).height() - $(selectors[i]['offset']).height()) / 2;
+			$(selectors[i]['selector']).css({
+				'padding-top': vertCenterPad + 'px',
+				'padding-bottom': vertCenterPad + 'px',
+			});
+
+			continue;
+		}
+
 		vertCenterPad = (windowHeight - $(selectors[i]).height()) / 2;
 		$(selectors[i]).css({
 			'padding-top': vertCenterPad + 'px',
-			'padding-bottom': vertCenterPad + 'px'
+			'padding-bottom': vertCenterPad + 'px',
 		});
 	}
 }
 
-function setFullFrame(element) {
+function setFullFrame(el) {
 	var windowHeight = $(window).height();
 
-	$(element).css('height', windowHeight + 'px');
-}
-
-function panelSet(status, parameters) {
-	if (status === true) {
-
-		$(parameters.container).panelSnap('enable');
-
-		// setFullFrame('.our-platform-placeholder, .content.our-platform');
-
-		setStickyRange();
-
-		if ((window.scrollY + $(window).height()) > stickyBottom) {
-			// console.log('scrollY + height: ' + (window.scrollY + $(window).height()));
-			// console.log('stickyBottom: ' + stickyBottom);
-			$(parameters.container).panelSnap('disable');
-			// panelSet(false, panelSetParameters);
-		}
-
-		// $(parameters.selectors).css('min-height', windowHeight + 'px');
-		// $(parameters.container).css({
-		// 	'height': windowHeight + 'px',
-		// 	'overflow-x': 'hidden'
-		// })
-		// .panelSnap('enable');
-
-		return;
+	if (typeof el === 'string') {
+		$(el).css('height', windowHeight + 'px');
 	}
-
-	$(parameters.selectors).attr('style', '');
-
-	$(parameters.container).attr('style', '').panelSnap('disable');
+	else {
+		for (var i = 0; i < el.length; i++) {
+			$(el[i]).css('height', windowHeight + 'px');
+		};
+	}
 }
 
 // Check for if it's in between the two and enable/disable accordingly
 // No need to do position: fixed
+
+$(document).ready(function() {
+
+	enquire.register('screen and (min-width: 1180px)', {
+		setup: function() {
+			setFullFrame(fullFrameSelectors);
+			$('.content.our-platform')
+				.panelSnap({
+					panelSelector: '> .container',
+					directionThreshold: 20,
+					slideSpeed: 400,
+				});
+		},
+		match: function() {
+			setFullFrame(fullFrameSelectors);
+			$('.content.our-platform').panelSnap('enable');
+
+		},
+		unmatch: function() {
+			setFullFrame(fullFrameSelectors, true);
+			$('.content.our-platform').panelSnap('disable');
+		},
+	});
+
+});
 
 $(window).load(function() {
 
 	enquire.register('screen and (min-width: 1180px)', {
 		deferSetup: true,
 		setup: function() {
-			verticalCenter(vertCenterSelectors);
-			$(panelSetParameters.container).panelSnap({
-				panelSelector: '> .container',
-				directionThreshold: 20,
-				slideSpeed: 400,
-			});
-			panelSet(false, panelSetParameters);
+			setVerticalCenter(vertCenterSelectors);
+			$('.content.our-platform .hero .copy').fadeIn();
 		},
 		match: function() {
-			verticalCenter(vertCenterSelectors);
-			$(window).scroll(function() {
-				if ((window.scrollY + $(window).height()) > $('.our-platform-end').position().top) {
-					console.log('disable');
-					$(panelSetParameters.container).panelSnap('disable');
-				}
-				else if (window.scrollY > $('.header-container').height()) {
-					console.log('enable');
-					$(panelSetParameters.container).panelSnap('enable');
-				}
-			});
-
+			setVerticalCenter(vertCenterSelectors);
 		},
 		unmatch: function() {
-			panelSetup(true, panelSetParameters);
-			verticalCenter(vertCenterSelectors, true);
+			setVerticalCenter(vertCenterSelectors, true);
 		},
 	});
 
-
-
-	// for (var i = 0; i < pagesLength; i++) {
-	// 	pages[i].top = $(pages[i].id).position().top;
-	// }
-
-
-	// Panel Snap setup
-
-
 });
-
-
-// ------------------------------------
-// Scrolling pagination
-// ------------------------------------
-/*
-// Global variables
-var scrollLast = 0;
-var scrollCurrent = 0;
-var scrollDirection = false;
-var scrollBottom = 0;
-
-// Cribbed scroll velocity detector, to deal with inertia scrolling
-var scrollVelocity = 0;
-var velocityRating = 1;
-function calculateVelocity() {
-    scrollLast = scrollCurrent;
-    scrollCurrent = $('#content').scrollTop();
-    scrollVelocity = scrollLast - scrollCurrent;
-    if (scrollVelocity > 20 || scrollVelocity < -20) {
-        velocityRating = 5;
-    } else {
-        velocityRating = 1;
-    }
-}
-
-$('#content').scroll(function () {
-    // get velocity while scrolling...
-    calculateVelocity();
-    // wait until finished scrolling...
-    clearTimeout($.data(this, 'scrollTimer'));
-    $.data(this, 'scrollTimer', setTimeout(function() {
-        // do your animation
-        $('#content').animate({scrollTop: snapPoint}, 300);
-    }, 300*velocityRating)); // short or long duration...
-});
-
-
-// could probably elegantly convert this using the debounce plugin?
-$(function() {
-	var timer;
-	$(window).scroll(function() {
-		scrollCheck = $(this).scrollTop();
-		scrollDistance = scrollCheck - scrollLast;
-		scrollLast = scrollCheck;
-    	clearTimeout(timer);
-    	timer = setTimeout(function() {
-      		$(window).trigger('scrollStop');
-    	}, 250);
-    	scrollLast = scrollCheck;
-  	});
-});
-
-$(window).on('touchmove', function() {
-	console.log('touchmove triggered');
-});
-
-// $(window).bind('touchmove', function() {
-// 	console.log('moving!');
-// })
-
-$(window).bind('scrollStop', function() {
-	var currentPage = {};
-
-	function scrollCheckPage() {
-		for (var i = 0; i < pagesLength; i++) {
-			if (pages[i].top === null) {
-				return;
-			}
-			if (window.scrollY < pages[i].top) {
-				if ((i - 1) > -1) {
-					currentPage = pages[i - 1];
-				}
-			}
-			break;
-		}
-	}
-
-	// if (scrollDirection === 'down') {
-
-	// }
-	// else if (scrollDirection === 'up') {
-
-	// }
-	// else {
-	// 	// don't change the page, but align it if we're on a page
-	// }
-
-	for (var i = 0; i < pagesLength; i++) {
-		if (pages[i].top === null) {
-			return;
-		}
-		if (window.scrollY < pages[i].top) {
-			// window.scrollTo(0, currentPage.top);
-			if ((i - 1) > -1) {
-				currentPage = pages[i - 1];
-			}
-			break;
-		}
-		else {
-			if (window.scrollY >= pages[i].top) {
-				// console.log(scrollDirection + ' ' + (window.scrollY - pages[i].top));
-				currentPage = pages[i];
-			}
-			else {
-				continue;
-			}
-		}
-	}
-	if ($.isEmptyObject(currentPage)) {
-		return;
-	}
-	else {
-		window.scrollTo(0, currentPage.top);
-	}
-
-});
-*/
-
-/*
-
-mimic OSX PDF preview scroll behavior:
-	check scroll direction, see if top/bottom is in a certain area within a certain number of pixels, then scroll to that element
-
-*/
