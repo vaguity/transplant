de76cdd5400a4fe1cdd01cc02411f2f282df93a8
Sean Connolly <sean@percolate.com>
2015-02-19 12:08:44 -0500

    Add video hero styling, move responsive resizing functions into transplant.js

--- dist/assets/js/our-platform.js
+++ dist/assets/js/our-platform.js
@@ -1,6 +1,6 @@
 // Set up offsets for full frame calculations as well?
 
-var fullFrameSelectors = [
+fullFrameSelectors = fullFrameSelectors.concat([
 	'.content.our-platform',
 	'.content.our-platform > .panel-container',
 	{
@@ -11,9 +11,9 @@ var fullFrameSelectors = [
 		'selector': '.content.our-platform > .panel-container-offset',
 		'offset': '.sticky',
 	},
-];
+]);
 
-var vertCenterSelectors = [
+vertCenterSelectors = vertCenterSelectors.concat([
 	{
 		'selector': '#customize .copy.right',
 		'offset': '.sticky',
@@ -50,78 +50,13 @@ var vertCenterSelectors = [
 		'selector': '.hero-container section',
 		'offset': '.header-container',
 	},
-];
+]);
 
-function setVerticalCenter(selectors, reset) {
-
-	var vertCenterLength = selectors.length;
-	var vertCenterPad = 0;
-
-	if (reset === true) {
-		for (var i = 0; i < vertCenterLength; i++) {
-			$(selectors[i]).attr('style', '');
-		}
-		return;
-	}
-
-	var windowHeight = $(this).height();
-
-	for (var i = 0; i < vertCenterLength; i++) {
-		if (typeof selectors[i] === 'object') {
-			vertCenterPad = (windowHeight - $(selectors[i]['selector']).height() - $(selectors[i]['offset']).outerHeight()) / 2;
-			if (vertCenterPad < 0) {
-				vertCenterPad = 0;
-				$(selectors[i]['selector']).css({
-					'padding-top': '',
-					'padding-bottom': '',
-				});
-			}
-			else {
-				$(selectors[i]['selector']).css({
-					'padding-top': vertCenterPad + 'px',
-					'padding-bottom': vertCenterPad + 'px',
-				});
-			}
-			continue;
-		}
-
-		vertCenterPad = (windowHeight - $(selectors[i]).height()) / 2;
-		$(selectors[i]).css({
-			'padding-top': vertCenterPad + 'px',
-			'padding-bottom': vertCenterPad + 'px',
-		});
-	}
-}
-
-function setFullFrame(el, reset) {
-	if (reset === true) {
-		var windowHeight = '';
-	}
-	else {
-		var windowHeight = $(window).height();
-	}
-
-	if (typeof el === 'string') {
-		$(el).css('height', windowHeight);
-	}
-	else {
-		for (var i = 0; i < el.length; i++) {
-			if (typeof el[i] === 'object') {
-				$(el[i]['selector']).css('height', (windowHeight - $(el[i]['offset']).outerHeight()) + 'px');
-			}
-			else {
-				$(el[i]).css('height', windowHeight + 'px');
-			}
-		}
-	}
-}
 
 $(document).ready(function() {
 	enquire.register('screen and (min-width: 1180px)', {
 		deferSetup: true,
 		setup: function() {
-			setFullFrame(fullFrameSelectors);
-			setVerticalCenter(vertCenterSelectors);
 			$('.content.our-platform')
 				.panelSnap({
 					panelSelector: '> .panel-container',
@@ -131,12 +66,10 @@ $(document).ready(function() {
 				});
 		},
 		match: function() {
-			setFullFrame(fullFrameSelectors);
 			$('.content.our-platform').panelSnap('enable');
 
 		},
 		unmatch: function() {
-			setFullFrame(fullFrameSelectors, true);
 			$('.content.our-platform').panelSnap('disable');
 		},
 	});
@@ -147,12 +80,7 @@ $(window).load(function() {
 	enquire.register('screen and (min-width: 1180px)', {
 		deferSetup: true,
 		setup: function() {
-			setVerticalCenter(vertCenterSelectors);
 			$('.content.our-platform .hero .copy').fadeIn();
-			$(window).resize($.throttle(150, function() {
-				setVerticalCenter(vertCenterSelectors);
-				setFullFrame(fullFrameSelectors);
-			}));
 			$('.content.our-platform').scroll($.throttle(150, function() {
 				if ($('.content.our-platform').scrollTop() >= ($(window).height() - $('.sticky').outerHeight())) {
 					$('.sticky').addClass('enabled').css('display', 'block');
@@ -162,12 +90,7 @@ $(window).load(function() {
 				}
 			}));
 		},
-		match: function() {
-			setVerticalCenter(vertCenterSelectors);
-		},
 		unmatch: function() {
-			setFullFrame(fullFrameSelectors, true);
-			setVerticalCenter(vertCenterSelectors, true);
 			$('.content.our-platform').panelSnap('disable');
 		},
 	});
--- dist/assets/js/transplant.js
+++ dist/assets/js/transplant.js
@@ -13,6 +13,10 @@ if (!mediaQueryCheck) {
 
 var footerPadding;
 var stickyTop, stickyBottom, stickySections, stickySection, stickySectionNew;
+var videoHeroRatio;
+var fullFrameSelectors = [];
+var vertCenterSelectors = [];
+
 
 function stickySetup() {
 	if ($('.sticky-begin').length) {
@@ -77,9 +81,123 @@ function stickyNav(set) {
 	}
 }
 
+function videoHeroSetup() {
+	if ($('.wistia-embed').length) {
+		var videoHeroWidth = parseInt($('.wistia-embed').css('width'));
+		var videoHeroHeight = parseInt($('.wistia-embed').css('height'));
+		videoHeroRatio = videoHeroWidth / videoHeroHeight;
+	}
+}
+
+function videoHero(set) {
+	if (set === true) {
+		if ($('.wistia-embed').length) {
+			var windowWidth = $(window).width();
+			var newHeroHeight = parseInt(windowWidth / videoHeroRatio);
+			$('.wistia-embed').css({
+				'width': windowWidth + 'px',
+				'height': newHeroHeight + 'px',
+			});
+			$('.hero-video-container .mask').css('height', newHeroHeight + 'px');
+			setVerticalCenter([{
+				'selector': '.hero-video-container h1',
+				'offset': $(window).height() - newHeroHeight,
+			}]);
+		}
+	}
+}
+
+function setVerticalCenter(selectors, reset) {
+
+	var vertCenterLength = selectors.length;
+	var vertCenterPad = 0;
+	if (vertCenterLength < 1) {
+		return;
+	}
+
+	if (reset === true) {
+		for (var i = 0; i < vertCenterLength; i++) {
+			$(selectors[i]).attr('style', '');
+		}
+		return;
+	}
+
+	var windowHeight = $(window).height();
+
+	for (var i = 0; i < vertCenterLength; i++) {
+		if (typeof selectors[i] === 'object') {
+			if (isNaN(selectors[i]['offset']) === true) {
+				offsetValue = $(selectors[i]['offset']).outerHeight();
+			}
+			else {
+				offsetValue = selectors[i]['offset'];
+			}
+			vertCenterPad = (windowHeight - $(selectors[i]['selector']).height() - offsetValue) / 2;
+			if (vertCenterPad < 0) {
+				vertCenterPad = 0;
+				$(selectors[i]['selector']).css({
+					'padding-top': '',
+					'padding-bottom': '',
+				});
+			}
+			else {
+				$(selectors[i]['selector']).css({
+					'padding-top': vertCenterPad + 'px',
+					'padding-bottom': vertCenterPad + 'px',
+				});
+			}
+			continue;
+		}
+
+		vertCenterPad = (windowHeight - $(selectors).height()) / 2;
+		$(selectors).css({
+			'padding-top': vertCenterPad + 'px',
+			'padding-bottom': vertCenterPad + 'px',
+		});
+	}
+}
+
+function setFullFrame(el, reset) {
+	if (reset === true) {
+		var windowHeight = '';
+	}
+	else {
+		var windowHeight = $(window).height();
+	}
+
+	if (typeof el === 'string') {
+		$(el).css('height', windowHeight);
+	}
+	else {
+		for (var i = 0; i < el.length; i++) {
+			if (typeof el[i] === 'object') {
+				$(el[i]['selector']).css('height', (windowHeight - $(el[i]['offset']).outerHeight()) + 'px');
+			}
+			else {
+				$(el[i]).css('height', windowHeight + 'px');
+			}
+		}
+	}
+}
+
 $(document).ready(function() {
 	$('.nav-icon').click(function() {
 		$('.primary-nav, .secondary-nav').toggle();
+	});
+
+	enquire.register('screen and (min-width: 1180px)', {
+		deferSetup: true,
+		setup: function() {
+			setFullFrame(fullFrameSelectors);
+			setVerticalCenter(vertCenterSelectors);
+		},
+		match: function() {
+			setFullFrame(fullFrameSelectors);
+
+		},
+		unmatch: function() {
+			setFullFrame(fullFrameSelectors, true);
+		},
 	});
 });
 
@@ -91,15 +209,36 @@ $(window).load(function() {
 		}
 	}
 	stickySetup();
+	videoHeroSetup();
+	videoHero(true);
+
+	enquire.register('screen and (min-width: 1180px)', {
+		deferSetup: true,
+		setup: function() {
+			setVerticalCenter(vertCenterSelectors);
+			$(window).resize($.throttle(150, function() {
+				setVerticalCenter(vertCenterSelectors);
+				setFullFrame(fullFrameSelectors);
+			}));
+		},
+		match: function() {
+			setVerticalCenter(vertCenterSelectors);
+		},
+		unmatch: function() {
+			setFullFrame(fullFrameSelectors, true);
+			setVerticalCenter(vertCenterSelectors, true);
+		},
+	});
 });
 
 $(window).on('resize', $.throttle(300, function() {
 	stickySetup();
+	videoHeroSetup();
+	videoHero(true);
 }));
 
 enquire
 .register("screen and (min-width: 1000px)", {
-	// Handle switch between previously toggled nav and desktop breakpoint
 	setup: function() {
 		stickyCalc();
 	},
@@ -110,6 +249,14 @@ enquire
 	unmatch: function() {
 		$('.primary-nav, .secondary-nav').css('display', 'none');
 		stickyNav(false);
+	},
+})
+.register("screen and (min-width: 1180px)", {
+	match: function() {
+		videoHero(true);
+	},
+	unmatch: function() {
+		videoHero(false);
 	},
 })
 .register("screen and (min-width: 1000px) and (max-width: 1180px)", {
